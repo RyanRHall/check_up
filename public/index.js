a6d0274710550dfdd986142abd2335cff51549b0
Ryan Hall <hall.ryan.r@gmail.com>
2018-10-19 13:40:06 -0400

    working stage

--- public/index.js
+++ public/index.js
@@ -1,3 +1,13 @@
 function _changeSlider(slider) {
    $('#slider_output').html(slider.value)
 }
+
+function _handleSmileSelect(id) {
+  $(".smile-img").removeClass("selected")
+  $("#smile-" + id.toString()).addClass("selected")
+}
+
+function _handleYesSelect(id) {
+  $(".yes-img").removeClass("selected")
+  $("#yes-" + id.toString()).addClass("selected")
+}
